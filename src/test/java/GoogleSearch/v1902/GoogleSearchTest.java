762af4bbfa2c9a05fce6728bbbc2b05c9bc685ae
Oksana_D <dmytriv.oksana@gmail.com>
2016-02-19 22:22:19 +0200

    Google Search Selenium

---
+++ src/test/java/GoogleSearch/v1902/GoogleSearchTest.java
@@ -0,0 +1,76 @@
+package GoogleSearch.v1902;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+import static googletest.core.CustomConditions.sizeOf;
+import static org.openqa.selenium.support.ui.ExpectedConditions.textToBePresentInElementLocated;
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleContains;
+
+public class GoogleSearchTest {
+
+    static WebDriver driver;
+    static WebDriverWait wait;
+
+    @BeforeClass
+    public static void setUp() {
+        driver = new FirefoxDriver();
+        wait = new WebDriverWait(driver, 6);
+    }
+
+    @AfterClass
+    public static void teardown() {
+        driver.quit();
+    }
+
+    @Test
+    public void testSearchText() {
+
+        openGoogle();
+        searchText("Selenium automates browsers");
+        assertResultsSize(10);
+        assertFirstResult("Selenium - Web Browser Automation");
+        followNthLink(0);
+        assertTitleOpenedURL("Selenium - Web Browser Automation");
+    }
+
+    public List<WebElement> results() {
+        return driver.findElements(By.cssSelector(".srg>.g"));
+    }
+
+    String results = (".srg>.g");
+
+    private void openGoogle() {
+        driver.get("http://google.com/ncr");
+    }
+
+    private void searchText(String searchText) {
+        driver.findElement(By.name("q")).sendKeys(searchText+Keys.ENTER);
+    }
+
+    private void assertResultsSize(int expectedSize) {
+        wait.until(sizeOf((By.cssSelector(results)), expectedSize));
+    }
+
+    private void assertFirstResult(String expectedText) {
+        wait.until(textToBePresentInElementLocated(By.cssSelector(results + ":nth-child(1)"), expectedText));
+    }
+
+    private void followNthLink(int index) {
+        results().get(index).findElement(By.cssSelector(".r>a")).click();
+    }
+
+    private void assertTitleOpenedURL(String expectedTitle) {
+        wait.until(titleContains(expectedTitle));
+    }
+}
+
