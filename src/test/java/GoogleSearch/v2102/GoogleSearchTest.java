253c8d714507b8722232c07eb8aa8df52d8c3ee6
Oksana_D <dmytriv.oksana@gmail.com>
2016-02-21 12:44:10 +0200

    Google Search separate followLink test final v2102_3

--- src/test/java/GoogleSearch/v2102/GoogleSearchTest.java
+++ src/test/java/GoogleSearch/v2102/GoogleSearchTest.java
@@ -4,6 +4,7 @@ import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -39,9 +40,11 @@ public class GoogleSearchTest {
         followNthLink(0);
         assertTitleOpenedURL("Selenium - Web Browser Automation");
     }
+    
     @Test
     public void testSearchCommonFlow() {
-        givenSearchResults("Selenium automates browsers");
+        openGoogle();
+        searchText("Selenium automates browsers");
         assertResultsSize(10);
         assertFirstResult("Selenium - Web Browser Automation");
     }
@@ -51,6 +54,15 @@ public class GoogleSearchTest {
     }
 
     String results = (".srg>.g");
+
+    private void openGoogle() {
+        driver.get("http://google.com/ncr");
+    }
+
+    private void searchText(String searchText) {
+        driver.findElement(By.name("q")).clear();
+        driver.findElement(By.name("q")).sendKeys(searchText+ Keys.ENTER);
+    }
 
     private void givenSearchResults(String request) {
         driver.get("https://www.google.com/?gws_rd=ssl#q=".concat(request));
